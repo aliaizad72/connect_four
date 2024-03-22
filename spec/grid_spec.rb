244ca4092f9a29fe62e4d737b05447a7d9e4b759
Ali Aizad <aliaizad72@gmail.com>
2024-03-22 15:01:24 +0800

    test: Write test for class method .in_grid?

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,6 +4,24 @@ require_relative '../lib/node'
 require_relative '../lib/grid'
 
 describe Grid do
+  describe '.in_grid?' do
+    it 'returns true if the coordinates is in the grid' do
+      valid_coordinates = [0, 0]
+      row = valid_coordinates[0]
+      column = valid_coordinates[1]
+      result = Grid.in_grid?(row, column)
+      expect(result).to be true
+    end
+
+    it 'returns false if the coordinates is not in the grid' do
+      invalid_coordinates = [-1, 0]
+      row = invalid_coordinates[0]
+      column = invalid_coordinates[1]
+      result = Grid.in_grid?(row, column)
+      expect(result).to be false
+    end
+  end
+
   describe '#create_matrix' do
     subject(:grid_matrix) { described_class.new }
     it 'creates an outer array of length 6' do
